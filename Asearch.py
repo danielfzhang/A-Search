1c283f90b036e9546017c39a19bec58997c3de51
danielfzhang <daniel11zhang@gmail.com>
2017-04-28 04:41:39 -0700

    V2.0
    read Maze, draw it in pygame (user can also draw blocks upon it)
    then find the shortest path from start to end
    need to optimize the program

--- Asearch.py
+++ Asearch.py
@@ -2,101 +2,104 @@
 
 #point is (x,y) coordinate
 #node is the structure, which contains point and additional info
-
 class Astar:
-   def __init__(self, startPoint, endPoint, blockPoints, size, step_cost):
-       #parameters initialization
-       self.__size=size#window size
-       self.__start=startPoint
-       self.__end=endPoint
-       self.__cost=step_cost
-       self.__openlist = []
-       self.__closelist = blockPoints
-       self.__findpath=False
-       self.shortestPath=[]
-       #calculate f() + g() + h()
-       #g() is the cost of the path from the start node to n
-       #h() is a heuristic that estimates the cost of the cheapest path from n to the goal
-       g=0
-       h=abs(self.__start[0]-self.__end[0])+abs(self.__start[1]-self.__end[1])
-       f=g+h
-       parentPoint=[-1,-1]#the start point has no parent point
-       #[0] (x,y), [1] is g(), [2] is h(),[3] is f(), [4] is parent
-       cur_node=[startPoint]+[g]+[h]+[f]+[parentPoint]
-       self.__openlist.append(cur_node)
-       #min_point trace the point with minimal f()
-       self.__minNode=cur_node
+    """
+    Perform A* algorithm to find the shortest path.
 
-   def __AddOpen(self, pre_node, expanded_point):
-       g=pre_node[1]+self.__cost
-       h=abs(expanded_point[0]-self.__end[0])+abs(expanded_point[1]-self.__end[1])
-       f=g+h
-       cur_node=[expanded_point]+[g]+[h]+[f] +[pre_node[0]]
-       #if the expanded point is in open list, do not add the point to open list
-       #if the cost of expanded point is less than the point in open list, replace it.
-       for i in range(len(self.__openlist)):
-           if self.__openlist[i][0]==expanded_point:
-               if f<self.__openlist[i][3]:
-                   self.__openlist[i]=cur_node
-               return
-       #if the expanded point is not in open list, add it
-       self.__openlist.append(cur_node)
+    find the shortest path from __start point to __end point
+    blockPoints are the points that cannot be positioned
+    size is the window size"""
+    def __init__(self, startPoint, endPoint, blockPoints, size, step_cost=1):
+        #parameters initialization
+        self.__start=startPoint
+        self.__end=endPoint
+        self.__closelist = blockPoints
+        self.__size=size#window size
+        self.__cost=step_cost
+        self.__openlist = []
+        self.__findpath=False
+        self.shortestPath=[]
+        #calculate f() + g() + h()
+        #g() is the cost of the path from the __start node to n
+        #h() is a heuristic that estimates the cost of the cheapest path from n to the goal
+        g=0
+        h=abs(self.__start[0]-self.__end[0])+abs(self.__start[1]-self.__end[1])
+        f=g+h
+        parentPoint=[-1,-1]#the __start point has no parent point
+        #[0] (x,y), [1] is g(), [2] is h(),[3] is f(), [4] is parent
+        cur_node=[startPoint]+[g]+[h]+[f]+[parentPoint]
+        #min_point trace the point with minimal f()
+        self.__minNode=cur_node
+        self.__openlist.append(cur_node)
 
-   def __RemoveNode(self, node):
-       #add the point to close list
-       if node[4]==[-1,-1]:
-           self.__closelist.append(node[0])
-       else:
-           for i in range(len(self.__closelist)):
-               if node[4][0]==self.__closelist[i][0] and node[4][1]==self.__closelist[i][1]:
-                   self.__closelist.append([node[0][0],node[0][1],i])
-                   break
-       #remove the point from open list
-       self.__openlist.remove(node)
-       #if there is element in the list
-       if self.__openlist!=[]:
-           #update min_point
-           self.__minNode=[[0,0]]+[0]+[3*self.__size]+[3*self.__size]
-           for each_point in self.__openlist:
-               if each_point[2]<=self.__minNode[2] and each_point[3]<=self.__minNode[3]:
-                   self.__minNode=each_point
+    def __AddOpen(self, expanded_point):
+        g=self.__minNode[1]+self.__cost
+        h=abs(expanded_point[0]-self.__end[0])+abs(expanded_point[1]-self.__end[1])
+        f=g+h
+        cur_node=[expanded_point]+[g]+[h]+[f]+[self.__minNode[0]]
+        #if the expanded point is in open list, do not add the point to open list
+        #if the cost of expanded point is less than the point in open list, replace it.
+        for node in self.__openlist:
+            if cur_node[0]==node[0]:
+                if cur_node[3]<node[3]:
+                    node=cur_node
+                return
+        #if the expanded point is not in open list, add it
+        self.__openlist.append(cur_node)
 
-   def __shortestPath(self):
-       cur_index=-1
-       self.shortestPath.insert(0,self.__end)
-       while len(self.__closelist[cur_index])!=2:
-           point=[self.__closelist[cur_index][0],self.__closelist[cur_index][1]]
-           self.shortestPath.insert(0,point)
-           cur_index=self.__closelist[cur_index][2]
-       self.shortestPath.insert(0,self.__start)
+    def __RemoveMin(self):
+        #add the point to close list
+        self.__closelist.append(self.__minNode)
+        #remove the point from open list
+        self.__openlist.remove(self.__minNode)
+        #if there is element in the list
+        if self.__openlist!=[]:
+            #update min_point
+            self.__minNode=[[0,0]]+[0]+[3*max(self.__size)]+[3*max(self.__size)]+[[-1,-1]]
+            for node in self.__openlist:
+                if node[2:4]<=self.__minNode[2:4]:
+                    self.__minNode=node
 
-   #only do search once, and return the point searched
-   def oneSearch(self):
-       if self.__findpath:
-           return "find path"
-       elif self.__openlist==[]:
-           return "no path"
-       else:
-           expanded_point=self.__minNode[0]
-           if expanded_point==self.__end:
-               self.__findpath=True
-               self.__shortestPath()
-           else:
-               direct4=[[0,-1],[0,1],[-1,0],[1,0]]#up,down,left,right
-               #add available points to open list,up,down,left,right
-               for direction in direct4:
-                   point_x=self.__minNode[0][0]+direction[0]
-                   point_y=self.__minNode[0][1]+direction[1]
-                   #the point is within the window
-                   if point_x>=0 and point_x<self.__size and point_y>=0 and point_y<self.__size:
-                       point=[point_x,point_y]
-                       duplicated=False
-                       for i in range(len(self.__closelist)):
-                           if point[0]==self.__closelist[i][0] and point[1]==self.__closelist[i][1]:
-                               duplicated=True
-                       #if the point is not in close list
-                       if not duplicated:
-                           self.__AddOpen(self.__minNode, point)
-           #remove the node
-           self.__RemoveNode(self.__minNode)
-           return expanded_point
+    def __shortestPath(self):
+        cur_index=-1
+        self.shortestPath.insert(0,self.__closelist[cur_index][0])
+        parent=self.__closelist[cur_index][4]
+        while parent!=[-1,-1]:
+            if self.__closelist[cur_index][0]==parent:
+                self.shortestPath.insert(0,parent)
+                parent=self.__closelist[cur_index][4]
+            cur_index-=1
+
+    #only do search once, and return the point searched
+    def oneSearch(self):
+        if self.__findpath:
+            return "find path"
+        elif self.__openlist==[]:
+            return "no path"
+        else:
+            expanded_point=self.__minNode[0][:]
+            if expanded_point==self.__end:
+                self.__findpath=True
+            else:
+                direct4=[[0,-1],[0,1],[-1,0],[1,0]]#up,down,left,right
+                #add available points to open list,up,down,left,right
+                for direction in direct4:
+                    point_x=self.__minNode[0][0]+direction[0]
+                    point_y=self.__minNode[0][1]+direction[1]
+                    #the point is within the window
+                    if 0<=point_x<self.__size[0] and 0<=point_y<self.__size[1]:
+                        point=[point_x,point_y]
+                        duplicated=False
+                        for node in self.__closelist:
+                            if point==node[0] or point==node:
+                                duplicated=True
+                                break
+                        #if the point is not in close list
+                        if not duplicated:
+                           self.__AddOpen(point)
+            #remove the node
+            self.__RemoveMin()
+            #find the shortest path, store it in the list
+            if self.__findpath:
+                self.__shortestPath()
+            return expanded_point
--- Game.py
+++ Game.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-from Asearch import Astar
+from asearch import Astar
+from analyzemaze import *
 import pygame
 import math
 from pygame.locals import *
 from sys import exit
 import time
-#searching parameters
-zoomin=6
-win_size=50
-start=[10,10]
-end=[45,45]
-blocks=[]
-step_cost=1
-delay=0.01
-#color definition
+#parameter definition
 WHITE=[255,255,255]
 RED=[255,0,0]
 BLACK=[0,0,0]
 BLUE=[141,182,205]
 GREEN=[0,255,127]
+Enlarge=6
+delay=0.01
+#analyze maze
+MazeSize,Start,End,BlockList=analyzeMaze('maze/2.PNG')
 #pygame initialization
 pygame.init()
-screen = pygame.display.set_mode((win_size*zoomin, win_size*zoomin), 0, 32)
+screen = pygame.display.set_mode((MazeSize[0]*Enlarge, MazeSize[1]*Enlarge), 0, 32)
 screen.fill(WHITE)
-pygame.draw.rect(screen, RED, (start[0]*zoomin, start[1]*zoomin,zoomin,zoomin))
-pygame.draw.rect(screen, RED, (end[0]*zoomin, end[1]*zoomin,zoomin,zoomin))
-pygame.display.set_caption("Find a path")
+pygame.display.set_caption("Maze")
+#draw blocks, start point and end point
+for pixel in BlockList:
+    pygame.draw.rect(screen,BLACK,[pixel[0]*Enlarge,pixel[1]*Enlarge,Enlarge,Enlarge])
+pygame.draw.rect(screen,RED,[Start[0]*Enlarge,Start[1]*Enlarge,Enlarge,Enlarge])
+pygame.draw.rect(screen,RED,[End[0]*Enlarge,End[1]*Enlarge,Enlarge,Enlarge])
 pygame.display.update()
 #flags
 buttondown=False
@@ -45,29 +45,29 @@ while True:
                     search=True
     if not DoSearch:
         if search:
-            Asearch=Astar(start,end,blocks,win_size,step_cost)
+            Asearch=Astar(Start,End,BlockList,MazeSize)
             while True:
                 result=Asearch.oneSearch()
                 if result!="find path" and result!="no path":
-                    cir_x=math.ceil(result[0]*zoomin+zoomin/2-1)
-                    cir_y=math.ceil(result[1]*zoomin+zoomin/2-1)
-                    pygame.draw.circle(screen, BLUE, (cir_x, cir_y), math.ceil(zoomin/2))
+                    cir_x=math.floor(result[0]*Enlarge+Enlarge/2-1)
+                    cir_y=math.floor(result[1]*Enlarge+Enlarge/2-1)
+                    pygame.draw.circle(screen, BLUE, (cir_x, cir_y), math.floor(Enlarge/2))
                     pygame.display.update()
                     time.sleep(delay)
                 else:
                     DoSearch=True
                     for p in Asearch.shortestPath:
-                        cir_x=math.ceil(p[0]*zoomin+zoomin/2-1)
-                        cir_y=math.ceil(p[1]*zoomin+zoomin/2-1)
-                        pygame.draw.circle(screen, GREEN, (cir_x, cir_y),math.ceil(zoomin/2))
+                        cir_x=math.floor(p[0]*Enlarge+Enlarge/2-1)
+                        cir_y=math.floor(p[1]*Enlarge+Enlarge/2-1)
+                        pygame.draw.circle(screen, GREEN, (cir_x, cir_y),math.floor(Enlarge/2))
                         pygame.display.update()
                         time.sleep(delay/2)
                     break
         elif buttondown:
             x, y = pygame.mouse.get_pos()
-            x=math.ceil(x/zoomin)
-            y=math.ceil(y/zoomin)
-            if [x,y] not in blocks:
-                blocks.append([x,y])
-            pygame.draw.rect(screen, BLACK, (x*zoomin, y*zoomin,zoomin,zoomin))
+            x=math.floor(x/Enlarge)
+            y=math.floor(y/Enlarge)
+            if [x,y] not in BlockList:
+                BlockList.append([x,y])
+            pygame.draw.rect(screen, BLACK, (x*Enlarge, y*Enlarge,Enlarge,Enlarge))
             pygame.display.update()
