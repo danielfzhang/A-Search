f88ad2c4b8c32c0344814ae5b550ff6f73660f66
danielfzhang <daniel11zhang@gmail.com>
2017-04-29 02:31:29 -0700

    V2.3
    moveable start

--- Game.py
+++ Game.py
@@ -1,70 +1,78 @@
 #!/usr/bin/python3
-from astar import Astar
-from analyzemaze import *
-import pygame
-import math
+from helper import *
 from pygame.locals import *
 from sys import exit
+import pygame, math, os
 #parameter definition
 WHITE=[255,255,255]
 RED=[255,0,0]
 BLACK=[0,0,0]
 BLUE=[141,182,205]
-Enlarge=4
+CADETBLUE=[152,245,255]
+Enlarge=28
 #analyze maze
 file= input("enter the file name of maze in folder maze:")
 file="maze/"+file
 MazeSize,Start,End,BlockList=analyzeMaze(file)
+#set the position of screen as (0,30)
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,30)
 #pygame initialization
 pygame.init()
 screen = pygame.display.set_mode((MazeSize[0]*Enlarge, MazeSize[1]*Enlarge), 0, 32)
-screen.fill(WHITE)
-pygame.display.set_caption("Maze")
-#draw blocks, start point and end point
-for pixel in BlockList:
-    pygame.draw.rect(screen,BLACK,[pixel[0]*Enlarge,pixel[1]*Enlarge,Enlarge,Enlarge])
-pygame.draw.rect(screen,RED,[Start[0]*Enlarge,Start[1]*Enlarge,Enlarge,Enlarge])
-pygame.draw.rect(screen,RED,[End[0]*Enlarge,End[1]*Enlarge,Enlarge,Enlarge])
-pygame.display.update()
+pygame.display.set_caption("Maze Defense")
+#draw Maze
+def drawMaze():
+    screen.fill(WHITE)
+    for pixel in BlockList:
+        pygame.draw.rect(screen,BLACK,[pixel[0]*Enlarge,pixel[1]*Enlarge,Enlarge,Enlarge])
+    pygame.draw.rect(screen,BLUE,[End[0]*Enlarge,End[1]*Enlarge,Enlarge,Enlarge])
+    pygame.draw.rect(screen,RED,[Start[0]*Enlarge,Start[1]*Enlarge,Enlarge,Enlarge])
+    pygame.display.update()
+drawMaze()
 #flags
-buttondown=False
-search=False
-DoSearch=False
+buttonDown=False
+showPath=False
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
-        elif not DoSearch:
-            if event.type ==  MOUSEBUTTONDOWN and not DoSearch:
-                buttondown=True
-            elif event.type ==  MOUSEBUTTONUP and not DoSearch:
-                buttondown=False
-            elif event.type == KEYDOWN and not DoSearch:
-                if event.key == K_RETURN:
-                    search=True
-    if not DoSearch:
-        if search:
-            Asearch=Astar(Start,End,BlockList,MazeSize)
-            while True:
+        elif event.type ==  MOUSEBUTTONDOWN:
+            buttonDown=True
+        elif event.type ==  MOUSEBUTTONUP:
+            buttonDown=False
+        elif event.type == KEYDOWN:
+            if event.key == K_RETURN:
+                Asearch=Astar(Start,End,BlockList,MazeSize)
                 result=Asearch.oneSearch()
-                if result!="find path" and result!="no path":
-                    cir_x=math.floor(result[0]*Enlarge+Enlarge/2-1)
-                    cir_y=math.floor(result[1]*Enlarge+Enlarge/2-1)
-                    pygame.draw.circle(screen, BLUE, (cir_x, cir_y), math.floor(Enlarge/2))
+                while result!="find path" and result!="no path":
+                    result=Asearch.oneSearch()
+                for p in Asearch.shortestPath:
+                    cir_x=math.floor(p[0]*Enlarge+Enlarge/2-1)
+                    cir_y=math.floor(p[1]*Enlarge+Enlarge/2-1)
+                    pygame.draw.circle(screen, CADETBLUE, (cir_x, cir_y),math.floor(Enlarge/2))
                     pygame.display.update()
-                else:
-                    DoSearch=True
-                    for p in Asearch.shortestPath:
-                        cir_x=math.floor(p[0]*Enlarge+Enlarge/2-1)
-                        cir_y=math.floor(p[1]*Enlarge+Enlarge/2-1)
-                        pygame.draw.circle(screen, RED, (cir_x, cir_y),math.floor(Enlarge/2))
-                        pygame.display.update()
-                    break
-        elif buttondown:
-            x, y = pygame.mouse.get_pos()
-            x=math.floor(x/Enlarge)
-            y=math.floor(y/Enlarge)
-            if [x,y] not in BlockList:
-                BlockList.append([x,y])
-            pygame.draw.rect(screen, BLACK, (x*Enlarge, y*Enlarge,Enlarge,Enlarge))
-            pygame.display.update()
+            elif event.key == K_UP:
+                if [Start[0],Start[1]-1] not in BlockList:
+                    Start[1]-=1
+                    drawMaze()
+            elif event.key == K_DOWN:
+                if [Start[0],Start[1]+1] not in BlockList:
+                    Start[1]+=1
+                    drawMaze()
+            elif event.key == K_RIGHT:
+                if [Start[0]+1,Start[1]] not in BlockList:
+                    Start[0]+=1
+                    drawMaze()
+            elif event.key == K_LEFT:
+                if [Start[0]-1,Start[1]] not in BlockList:
+                    Start[0]-=1
+                    drawMaze()
+
+    if buttonDown:
+        x, y = pygame.mouse.get_pos()
+        x=math.floor(x/Enlarge)
+        y=math.floor(y/Enlarge)
+        if [x,y] not in BlockList:
+            BlockList.append([x,y])
+        pygame.draw.rect(screen, BLACK, (x*Enlarge, y*Enlarge,Enlarge,Enlarge))
+        pygame.display.update()
--- helper.py
+++ helper.py
@@ -1,4 +1,61 @@
 #!/usr/bin/python3
+import cv2
+import numpy as np
+import math
+def analyzeMaze(file):
+    """Analyze an image of a Maze.
+
+    return MazeSize, Start point, Endpoint, BlockList"""
+    #color definition
+    WHITE=np.array([255,255,255])
+    BLACK=np.array([0,0,0])
+    #read the image file
+    img = cv2.imread(file)
+    #find origin_Maze,,blk_size
+    blk_size=0
+    findMaze=False
+    findblk_size=False
+    for y,row in enumerate(img):
+        for x,pixel in enumerate(row):
+            if (pixel==BLACK).all():
+                origin_Maze=[y,x]
+                findMaze=True
+                break
+        if findMaze:
+            break
+    for i in range(origin_Maze[0],img.shape[0]):
+        if (img[i][origin_Maze[1]]==WHITE).all():
+            blk_size+=1
+        elif (pixel!=BLACK).all():
+            break
+    #compute block list
+    BlockList=[]
+    for j in range(0,2):
+        for y, row in enumerate(img[origin_Maze[0]+j:-1:blk_size+1]):
+            for i in range(0,2):
+                for x, pixel in enumerate(row[origin_Maze[1]+i:-1:blk_size+1]):
+                    if (pixel==BLACK).all():
+                        BlockList.append([y*2+j,x*2+i])
+    #the structure of image is [y][x], but it is [x][y] in pygame
+    #we need to switch the x and y for Start,End,BlockList
+    BlockList.sort()
+    y=max(BlockList)[0]
+    for e in BlockList:
+        exch=e[0]
+        e[0]=e[1]
+        e[1]=exch
+    x=max(BlockList)[0]
+    MazeSize=[x+1,y+1]
+    #find Start and End point
+    for i in range(MazeSize[1]):
+        if [0,i] not in BlockList:
+            Start=[0,i]
+            break
+    for i in range(MazeSize[0]):
+        if [MazeSize[0]-1,i] not in BlockList:
+            End=[MazeSize[0]-1,i]
+            break
+    return MazeSize,Start,End,BlockList
 
 #point is (x,y) coordinate
 #node is the structure, which contains point and additional info
@@ -13,7 +70,7 @@ class Astar:
         #parameters initialization
         self.__start=startPoint
         self.__end=endPoint
-        self.__closelist = blockPoints
+        self.__closelist = blockPoints[:]
         self.__size=size#window size
         self.__cost=step_cost
         self.__openlist = []
