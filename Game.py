2eae54390312540f5c88bf520df064ef3799e743
danielfzhang <daniel11zhang@gmail.com>
2017-05-02 18:12:37 -0700

    show

--- Game.py
+++ Game.py
@@ -10,7 +10,9 @@ Black=[0,0,0]
 Red=[255,0,0]
 Green=[0,255,0]
 #load,analyze images
-mazeSize,Start,End,blockList=analyzeMaze('img/maze1.PNG')
+filename=random.randint(1, 4)
+filename='img/maze' + str(filename)+'.PNG'
+mazeSize,Start,End,blockList=analyzeMaze(filename)
 #window set up
 monsterPos=[-1,-1]
 viewPoint=[0,mazeSize[1]//2]
@@ -44,7 +46,9 @@ def updateMap():
     pygame.draw.rect(screen,Green,[p[0]*miniMapSize+p[1] for p in zip(End,Shifting)]+[miniMapSize,miniMapSize])
     pygame.draw.rect(screen,Green,[p[0]*miniMapSize+p[1] for p in zip(Start,Shifting)]+[miniMapSize,miniMapSize])
     pygame.draw.rect(screen,Red,[p[0]*miniMapSize+p[1] for p in zip(monsterPos,Shifting)]+[miniMapSize,miniMapSize])
-    pygame.display.update()
+    miniWin=[viewPoint[0]-MBnum[0]//2,viewPoint[1]-MBnum[1]//2]
+    pygame.draw.rect(screen,Red,[p[0]*miniMapSize+p[1] for p in zip(miniWin,Shifting)]+[MBnum[0]*miniMapSize,MBnum[1]*miniMapSize],2)
+    pygame.display.flip()
 def geneMonster(destination):
     global monsterPos, monsterMove
     monsterPos=End[:]
@@ -67,6 +71,7 @@ def geneMonster(destination):
                         randmove.append(point)
             nextStp=random.choice(randmove)
             preStep=[[],[]]
+            pygame.time.delay(monsterSpeed*3)
         else: preStep.append(nextStp)
 
         if nextStp==None: pass
@@ -83,41 +88,49 @@ monsterMove=False
 leftClick=False
 rightClick=False
 #pygame main loop
+clock = pygame.time.Clock()
 while True:
     for event in pygame.event.get():#detect keyboard, mouse inputs
         if event.type == QUIT: exit()
-        elif event.type ==  MOUSEBUTTONDOWN:
-            pressed_mouse = pygame.mouse.get_pressed()
-            if pressed_mouse[0]:
-                leftClick=True
-            elif pressed_mouse[2]: rightClick=True
-        elif event.type ==  MOUSEBUTTONUP:
-            leftClick=False
-            rightClick=False
-        elif event.type == KEYDOWN:
-            if event.key == K_RETURN and not monsterMove:#generage monster
-                monsterMove=True
-                t1=threading.Thread(target=geneMonster,args=(Start,))
-                t1.setDaemon(True)
-                t1.start()
-    #functions after something is inputted
-    if leftClick or rightClick:#click mouse to move map, set blocks or remove blocks
+    #(mouse functions-----
+    pressed_mouse = pygame.mouse.get_pressed()
+    if pressed_mouse[0] or pressed_mouse[2]:
         x, y = pygame.mouse.get_pos()
+        #click minimap to move map
         if Shifting[0]<=x<miniMapSize*mazeSize[0] and Shifting[1]<=y<miniMapSize*mazeSize[1]:
-            if leftClick:
+            if pressed_mouse[0]:
                 x=(x-Shifting[0])//miniMapSize
                 y=(y-Shifting[1])//miniMapSize
                 viewPoint=[x,y]
         else:
-            if leftClick:
+            if pressed_mouse[0]:#click left mouse button to set block
                 x=(x-Shifting[0])//MBsize-MBnum[0]//2+viewPoint[0]
                 y=(y-Shifting[1])//MBsize-MBnum[1]//2+viewPoint[1]
                 if 0<x<mazeSize[0]-1 and 0<y<mazeSize[1]-1:
                     if [x,y] not in blockList: blockList.append([x,y])
-            else:
+            else:#click right mouse button to remove block
                 x, y = pygame.mouse.get_pos()
                 x=(x-Shifting[0])//MBsize-MBnum[0]//2+viewPoint[0]
                 y=(y-Shifting[1])//MBsize-MBnum[1]//2+viewPoint[1]
                 if 0<x<mazeSize[0]-1 and 0<y<mazeSize[1]-1:
                     if [x,y] in blockList: blockList.remove([x,y])
+    #----------)
+    #(keyboard functions-----
+    pressed_key = pygame.key.get_pressed()
+    if pressed_key[pygame.K_RETURN] and not monsterMove:#generage monster
+        monsterMove=True
+        t1=threading.Thread(target=geneMonster,args=(Start,))
+        t1.setDaemon(True)
+        t1.start()
+    #move view point
+    elif pressed_key[pygame.K_UP]:
+        if viewPoint[1]>Shifting[1]: viewPoint[1]-=1
+    elif pressed_key[pygame.K_DOWN]:
+        if viewPoint[1]<Shifting[1]+mazeSize[1]: viewPoint[1]+=1
+    elif pressed_key[pygame.K_RIGHT]:
+        if viewPoint[0]<Shifting[0]+mazeSize[0]: viewPoint[0]+=1
+    elif pressed_key[pygame.K_LEFT]:
+        if viewPoint[0]>Shifting[0]: viewPoint[0]-=1
+    #------------)
     updateMap()
+    clock.tick(60)
