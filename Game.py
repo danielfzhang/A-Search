d2ef3c86a47d3f7bcad8b5f68021d1d8dba9f276
danielfzhang <daniel11zhang@gmail.com>
2017-05-02 17:09:44 -0700

    click miniMap to move Map, generate monster

--- Game.py
+++ Game.py
@@ -2,41 +2,41 @@
 from helper import *
 from pygame.locals import *
 from sys import exit
-import pygame, os, threading, random
-#parameters
-MBsize=70
-MBnum=12
-miniMapSize=3
-windowSize=(1300, MBsize*MBnum)
-moveSpeed=50
-Shifting=[30,0]
+import pygame, os, threading, random, math
 #colors
 Grey=[105,105,105]
 White=[255,255,255]
 Black=[0,0,0]
 Red=[255,0,0]
 Green=[0,255,0]
+#load,analyze images
+mazeSize,Start,End,blockList=analyzeMaze('img/maze1.PNG')
 #window set up
+monsterPos=[-1,-1]
+viewPoint=[0,mazeSize[1]//2]
+Shifting=[0,0]
+MBsize=50
+MBnum=[24,14]
+miniMapSize=5
+monsterSpeed=100
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,30)
+windowSize=(Shifting[0]+MBsize*MBnum[0], MBsize*MBnum[1])
 pygame.init()
 screen = pygame.display.set_mode(windowSize, 0, 32)
 pygame.display.set_caption("Maze Defense")
-#load, and analyze images
-mazeSize,Start,End,blockList=analyzeMaze('img/maze1.PNG')
 Monster=pygame.image.load('img/monster.PNG').convert_alpha()
 Monster = pygame.transform.scale(Monster, (MBsize, MBsize))
-monsterPos=End[:]
-#functions-----------
+#(functions-----------
 def updateMap():
-    #draw Map
+    global viewPoint
     screen.fill(White)
-    lowbound=MBnum//2*-1
-    uppbound=(MBnum+1)//2
-    for x in range(lowbound,uppbound):
-        for y in range(lowbound,uppbound):
-            if [monsterPos[0]+x,monsterPos[1]+y] in blockList:
-                pygame.draw.rect(screen,Black,[(x+MBnum//2)*MBsize+Shifting[0],(y+MBnum//2)*MBsize+Shifting[1],MBsize,MBsize])
-    else: screen.blit(Monster, [MBnum//2*MBsize+Shifting[0],MBnum//2*MBsize+Shifting[1]])
+    for x in range(MBnum[0]//2*-1,MBnum[0]//2):#draw rows
+        for y in range(MBnum[1]//2*-1,MBnum[1]//2):#draw cols
+            if [viewPoint[0]+x,viewPoint[1]+y] in blockList:#draw blocks
+                pygame.draw.rect(screen,Black,[(x+MBnum[0]//2)*MBsize+Shifting[0],(y+MBnum[1]//2)*MBsize+Shifting[1],MBsize,MBsize])
+            elif [viewPoint[0]+x,viewPoint[1]+y]==monsterPos:#draw monster
+                if monsterPos!=[-1,-1]:
+                    screen.blit(Monster, [(x+MBnum[0]//2)*MBsize+Shifting[0],(y+MBnum[1]//2)*MBsize+Shifting[1]])
     #draw minimap
     pygame.draw.rect(screen,White,Shifting+[mazeSize[0]*miniMapSize,mazeSize[1]*miniMapSize])
     for pixel in blockList:
@@ -45,12 +45,11 @@ def updateMap():
     pygame.draw.rect(screen,Green,[p[0]*miniMapSize+p[1] for p in zip(Start,Shifting)]+[miniMapSize,miniMapSize])
     pygame.draw.rect(screen,Red,[p[0]*miniMapSize+p[1] for p in zip(monsterPos,Shifting)]+[miniMapSize,miniMapSize])
     pygame.display.update()
-updateMap()
-#move to destination
-def toDestn(destination):
-    global autoMove, monsterPos
+def geneMonster(destination):
+    global monsterPos, monsterMove
+    monsterPos=End[:]
     preStep=[[],[]]#track last two steps
-    while autoMove:
+    while monsterMove:
         Asearch=Astar(monsterPos,destination,blockList,mazeSize)
         nextStp=Asearch.nextStep()
         deadlck=preStep.pop(0)
@@ -69,59 +68,56 @@ def toDestn(destination):
             nextStp=random.choice(randmove)
             preStep=[[],[]]
         else: preStep.append(nextStp)
-        if not autoMove: return#interrupted
+
         if nextStp==None: pass
         else:
             monsterPos=nextStp#move
-            pygame.time.delay(moveSpeed)
-            if nextStp==destination: autoMove=False#reach destination
+            pygame.time.delay(monsterSpeed)
+            if nextStp==destination:
+                monsterPos=[-1,-1]
+                monsterMove=False#reach destination
+#-------------functions)
+updateMap()
 #flags
-drawBLK=False
-drawWHT=False
-autoMove=False
-left=right=up=down=False
+monsterMove=False
+leftClick=False
+rightClick=False
 #pygame main loop
 while True:
-    for event in pygame.event.get():
+    for event in pygame.event.get():#detect keyboard, mouse inputs
         if event.type == QUIT: exit()
         elif event.type ==  MOUSEBUTTONDOWN:
             pressed_mouse = pygame.mouse.get_pressed()
-            if pressed_mouse[0]: drawBLK=True#left click to set blocks
-            elif pressed_mouse[2]: drawWHT=True#right click to remove blocks
+            if pressed_mouse[0]:
+                leftClick=True
+            elif pressed_mouse[2]: rightClick=True
         elif event.type ==  MOUSEBUTTONUP:
-            drawBLK=False
-            drawWHT=False
+            leftClick=False
+            rightClick=False
         elif event.type == KEYDOWN:
-            if event.key == K_RETURN and not autoMove:#automatically walk to destination
-                autoMove=True
-                t1=threading.Thread(target=toDestn,args=(Start,))
+            if event.key == K_RETURN and not monsterMove:#generage monster
+                monsterMove=True
+                t1=threading.Thread(target=geneMonster,args=(Start,))
                 t1.setDaemon(True)
                 t1.start()
+    #functions after something is inputted
+    if leftClick or rightClick:#click mouse to move map, set blocks or remove blocks
+        x, y = pygame.mouse.get_pos()
+        if Shifting[0]<=x<miniMapSize*mazeSize[0] and Shifting[1]<=y<miniMapSize*mazeSize[1]:
+            if leftClick:
+                x=(x-Shifting[0])//miniMapSize
+                y=(y-Shifting[1])//miniMapSize
+                viewPoint=[x,y]
+        else:
+            if leftClick:
+                x=(x-Shifting[0])//MBsize-MBnum[0]//2+viewPoint[0]
+                y=(y-Shifting[1])//MBsize-MBnum[1]//2+viewPoint[1]
+                if 0<x<mazeSize[0]-1 and 0<y<mazeSize[1]-1:
+                    if [x,y] not in blockList: blockList.append([x,y])
             else:
-                autoMove=False
-                #control monster by up, down, right, left arrow keys
-                if event.key == K_UP:
-                    if [monsterPos[0],monsterPos[1]-1] not in blockList:
-                        monsterPos[1]-=1
-                elif event.key == K_DOWN:
-                    if [monsterPos[0],monsterPos[1]+1] not in blockList:
-                        monsterPos[1]+=1
-                elif event.key == K_RIGHT:
-                    if [monsterPos[0]+1,monsterPos[1]] not in blockList:
-                        if monsterPos[0]+1<mazeSize[0]: monsterPos[0]+=1
-                elif event.key == K_LEFT:
-                    if [monsterPos[0]-1,monsterPos[1]] not in blockList:
-                        if monsterPos[0]>0: monsterPos[0]-=1
-    if drawBLK:#left click set blocks
-        x, y = pygame.mouse.get_pos()
-        x=(x-Shifting[0])//MBsize-MBnum//2+monsterPos[0]
-        y=(y-Shifting[1])//MBsize-MBnum//2+monsterPos[1]
-        if 0<x<mazeSize[0]-1 and 0<y<mazeSize[1]-1:
-            if [x,y] not in blockList: blockList.append([x,y])
-    elif drawWHT:#right click remove blocks
-        x, y = pygame.mouse.get_pos()
-        x=(x-Shifting[0])//MBsize-MBnum//2+monsterPos[0]
-        y=(y-Shifting[1])//MBsize-MBnum//2+monsterPos[1]
-        if 0<x<mazeSize[0]-1 and 0<y<mazeSize[1]-1:
-            if [x,y] in blockList: blockList.remove([x,y])
+                x, y = pygame.mouse.get_pos()
+                x=(x-Shifting[0])//MBsize-MBnum[0]//2+viewPoint[0]
+                y=(y-Shifting[1])//MBsize-MBnum[1]//2+viewPoint[1]
+                if 0<x<mazeSize[0]-1 and 0<y<mazeSize[1]-1:
+                    if [x,y] in blockList: blockList.remove([x,y])
     updateMap()
